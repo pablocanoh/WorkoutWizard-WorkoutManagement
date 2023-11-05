19eeda66f6ac6ff095355786231dde10ecf8e0e2
pablocanohueros <canohueros75@gmail.com>
2023-11-05 19:22:23 +0100

    first commit

---
+++ src/main/java/edu/uoc/workoutwizardworkoutmanagement/WorkoutWizardWorkoutManagementApplication.java
@@ -0,0 +1,13 @@
+package edu.uoc.workoutwizardworkoutmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutWizardWorkoutManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutWizardWorkoutManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/uoc/workoutwizardworkoutmanagement/WorkoutWizardWorkoutManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uoc.workoutwizardworkoutmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutWizardWorkoutManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
