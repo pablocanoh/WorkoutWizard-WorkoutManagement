ec31fe08d64d70501f3d86935c8b400811306906
pablocanohueros <canohueros75@gmail.com>
2023-12-10 17:12:19 +0100

    Add workout-core and workout-client as sub-projects
    Updated settings.gradle file to include these sub-projects, created both workout-core and workout-client with their necessary files including gradle settings and wrapper. Modified the main build for project splitting across sub-projects, with them having their own gradle configuration. These changes facilitate the management and scalability of the project in the future.

---
+++ workout-client/src/test/java/com/example/workoutclient/WorkoutClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workoutclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
